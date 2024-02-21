9f0bb963372f8d111b9202ae8edced9150b42ce7
amasoud12 <ahmadmasoud@my.unt.edu>
2024-02-21 10:55:29 -0600

    added a new py file - data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+# Data to be worked on is comprised of seven main categories
+
+# Date: time period we are working on 
+# High: highest price that a stock reached
+# Low: lowest price that a stock reached 
+# Open: price of stock soon as a market opens on a particular day
+# Volume: number of shares traded 
+# Closing Price: cost of shares agreed on by traders at the end of a particular day  
+# Adjusted Close: cost of share at the end of a day, taking into consediration stock splits, new stock offerings and dividens 
+
+# For this study, the critical datapoint we are most interested in and the pivot to this study is the Closing Price
+
